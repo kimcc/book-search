206c384d94f62563decde72eff4025635a55fe6b
kimcc <15278020+kimcc@users.noreply.github.com>
2021-11-20 23:50:59 -0600

    add server, mutations and queries

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,43 @@
+// Import gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// Create typedefs
+const typeDefs = gql`
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
+
+  type Mutation {
+    login(email: String!, password: String): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(authors: [String]!, description: String!, title: String!, bookId: ID!, image: String!, link: String!): User
+    removeBook(bookId: ID!): User
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
+// Export typedefs
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,21 +1,52 @@
 const express = require('express');
+const { ApolloServer } = require ('apollo-server-express');
 const path = require('path');
+
+const { ApolloServerPluginLandingPageGraphQLPlayground,
+  ApolloServerPluginLandingPageDisabled } = require('apollo-server-core');
+
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
-const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+async function startServer() {
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+    plugins: [ // Show GraphQL playground
+      process.env.NODE_ENV === 'production'
+      ? ApolloServerPluginLandingPageDisabled()
+      : ApolloServerPluginLandingPageGraphQLPlayground(),
+      ] 
+  })
+
+  // Serve up static assets
+  // if (process.env.NODE_ENV === 'production') {
+  //   app.use(express.static(path.join(__dirname, '../client/build')));
+  // }
+
+  await server.start();
+
+  server.applyMiddleware({ app });
 }
 
-app.use(routes);
+startServer();
+
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  });
 });
