f2eacc10da3d4d274c6869be6b30318152986522
kimcc <15278020+kimcc@users.noreply.github.com>
2021-11-22 04:18:25 -0600

    uncomment build for production

---
+++ client/build/precache-manifest.4d09663f3d47556e962f1ccd67c47a35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b931ca5593f9612ba7bb45c0e6f97566",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d97385fa8f21c1786664",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "a185303b94edac6dff6d",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "d97385fa8f21c1786664",
+    "url": "/static/js/2.8f721e52.chunk.js"
+  },
+  {
+    "revision": "457f54dd419eef7a4c562e845736f6fc",
+    "url": "/static/js/2.8f721e52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a185303b94edac6dff6d",
+    "url": "/static/js/main.28ebfff2.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
